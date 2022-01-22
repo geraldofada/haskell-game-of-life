2acc1fa10e0f77ef4d7da8dfe701c76621bb3ec2
Geraldo Fada <geraldofada.neto@gmail.com>
2022-01-22 15:08:12 -0300

    read file and parse input

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Control.Monad
+import System.Environment
+import System.IO
+
+main = do
+  args <- getArgs
+  let fileName = head args
+  let qtyIterations = args !! 1
+  tableInput <- readFile fileName
+  let table = lines tableInput
+  print $ head table
+
+-- A entrada consiste num txt do seguinte formato:
+--
+-- v........z.......
+-- v........z.......
+-- .........v.......
+-- .....v...........
+-- ............v....
+-- ........v........
+--
+-- Em que:
+-- "." : são as células mortas
+-- "v" : as células vivas
+-- "z" : as células zumbis
+
+
